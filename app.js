e897c3cfa382f55b99d6272c889cad1f6f2ffb5b
AHMEDSOOMRO <114793154+MukhtairAhmed@users.noreply.github.com>
2025-04-20 02:25:43 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+var valu = document.getElementById("val")
+
+
+function increment(){
+valu.innerHTML = parseInt(valu.innerHTML)+1;
+}
+
+function dre(){
+valu.innerHTML = parseInt(valu.innerHTML)-1;
+}
+
+function reset(){
+    valu.innerHTML = 0;
+}
